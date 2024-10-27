51823a76967b987f01ae20d03fb9a3d8f2b0a2c2
Marcos Vinicius Ferreira Paim <viniciusferreirapaim@yahoo.com>
2024-10-27 15:30:00 -0300

    métodos creditos e debitos

--- src/br/com/mhbank/calculosalario/controller/Creditos.java
+++ src/br/com/mhbank/calculosalario/controller/Creditos.java
@@ -1,7 +1,27 @@
-
 package br.com.mhbank.calculosalario.controller;
 
 public class Creditos {
-    public static double IRPF, INSS, VT, VR, convenioMedico, VA;
-    
+
+    public static double salario, bonus, horaExtra, total;
+
+    public static double HoraExtra(double salario, double jornadaTrabalho, double horasExtras) {
+
+        double salarioHora = salario / jornadaTrabalho;
+
+        horaExtra = ((salarioHora * 0.5) + salarioHora) * horasExtras;
+
+        return horaExtra;
+    }
+
+    public static double GanhosTotais(double salario, double bonusFuncionario, double horasExtras,
+            int jornadaTrabalho) {
+
+        horaExtra = Creditos.HoraExtra(salario, jornadaTrabalho, horasExtras);
+
+        bonus = bonusFuncionario;
+
+        total = salario + horaExtra + bonus;
+
+        return total;
+    }
 }
---
+++ src/br/com/mhbank/calculosalario/view/main.java
@@ -0,0 +1,11 @@
+
+package br.com.mhbank.calculosalario.view;
+
+import br.com.mhbank.calculosalario.controller.Debitos;
+
+public class main {
+    public static void main(String[] args) {
+        
+        System.out.println(Debitos.DescontoIRPF(2800, 1));
+    }
+}
