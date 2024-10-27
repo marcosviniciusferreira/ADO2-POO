6c0184c8d0a6ef5b2d379ec2f1b25e9bdf77c3d4
Marcos Vinicius Ferreira Paim <viniciusferreirapaim@yahoo.com>
2024-10-27 14:23:42 -0300

    estrutura projeto

---
+++ src/br/com/mhbank/calculosalario/controller/Creditos.java
@@ -0,0 +1,7 @@
+
+package br.com.mhbank.calculosalario.controller;
+
+public class Creditos {
+    public static double IRPF, INSS, VT, VR, convenioMedico, VA;
+    
+}
---
+++ src/br/com/mhbank/calculosalrio/model/Funcionario.java
@@ -0,0 +1,12 @@
+package br.com.mhbank.calculosalrio.model;
+
+public class Funcionario {
+    
+    static public String nome;
+    static public double salario, salarioTributavel, totalVT, totalVR, convenioMedico, totalVA, irpf, inss, horasExtras,
+			bonus;
+    
+    static public int quantDependente, jornadaTrabalho;
+    
+    
+}
